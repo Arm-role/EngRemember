a905bf4d45976d65984b6c310c58c0eeb62ffbbe
Armor <76558013+Arm-role@users.noreply.github.com>
2023-10-22 17:27:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
-let correctword = 0;
+let TScore = 0;
+let FScore = 0;
 let Words = [{eng:"apple", thai:"แอปเปิล"},
              {eng:"banana", thai:"กล้วย"},
-            ];
-
-let current = correctword;
+];
+let wrong = FScore;
+let correct = TScore;
 let text = "";
+let WordList = document.getElementById("wordslist")
+WordList.innerText = Words.length;
 ReWord()
 
 function getValue(){
     let getWord = document.getElementById("thaiWord")
     let WordInput = getWord.value;
     if(WordInput == text){
-        correctword ++;
-        getWord.value = "";
+        Correct();
     }
     else{
         Nottrue()
-    }
-    let score = document.getElementById("Score");
-    score.innerText = correctword;
-
-    if(current != correctword){
-        ReWord();
-        current = correctword;
     }
 }
 
@@ -33,8 +28,40 @@ function ReWord(){
     test.innerText = word.eng; 
     text = word.thai;
 }
-function Nottrue(){
-    console.log("No")
+function Correct(){
+    TScore ++;
     let getWord = document.getElementById("thaiWord")
     getWord.value = "";
+    let Tscore = document.getElementById("True");
+    Tscore.innerText = TScore;
+    if(correct != TScore){
+        ReWord();
+        correct = TScore;
+    }
+}
+function Nottrue(){
+    FScore ++;
+    let getWord = document.getElementById("thaiWord")
+    getWord.value = "";
+    let Fscore = document.getElementById("False");
+    Fscore.innerText = wrong;
+    if(wrong != FScore){
+        ReWord();
+        wrong = FScore;
+    }
+}
+
+function AddWord(){
+    console.log("add");
+    let InputThaiWord = document.getElementById("addthai").value
+    let InputEngWord = document.getElementById("addeng").value
+    console.log(InputThaiWord,InputEngWord);
+    if(InputThaiWord.trim() !== "" && InputEngWord.trim() !== ""){
+        Words.push({eng: InputEngWord,thai: InputThaiWord});
+        console.log(Words);
+        WordList.innerText = Words.length;
+        document.getElementById("addthai").value = "";
+        document.getElementById("addeng").value = "";
+    }
+    
 }
